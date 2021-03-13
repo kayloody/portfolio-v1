ac2a7de05a8668ed9715109fca1f32e12c85a79c
Khaled <kayloody@gmail.com>
2021-03-13 13:44:45 -0700

    Updated favicons and metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: `Khaled Al-Amoodi`,
+    siteUrl: `https://www.alamoodi.io`,
+    description: `I code for the web. I craft for humans.`,
+  },
 }
