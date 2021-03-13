0e27afd30213d53e79c4ebdc6bf2d28c2860012c
Khaled <kayloody@gmail.com>
2021-03-12 23:50:46 -0700

    Created Gatsby project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function IHome() {
+  return (
+    <div>
+      <h2>Stay tuned! Portfolio will be up soon.</h2>
+      <p>- Khaled Al-Amoodi</p>
+    </div>
+  )
 }
