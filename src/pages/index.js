ac1d2cafa3a6fbcbca2ec78f7f6daccf325cbc22
Khaled <kayloody@gmail.com>
2021-03-21 21:03:06 -0600

    Attempted fix of vh on mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,11 @@ import swftliMeImg from "../img/projects/swftli-me.jpg"
 export default function Home() {
   const [contactStatus, setContactStatus] = useState("")
 
+  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+  let vh = window.innerHeight * 0.01
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`)
+
   const sendEmail = e => {
     e.preventDefault()
 
